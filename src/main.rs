a7c9bdbf56a11e4ab7cf418600b8d13972b8ef26
giri <giri@brahmastra.dev>
2022-08-27 08:01:10 +0530

    install wg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self, BufRead};
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Wireguard Management App");
+    println!("Please select an option:");
+
+    let mut command = String::new();
+    let stdin = io::stdin();
+    stdin.lock().read_line(&mut command).unwrap();
+    let command = command.trim();
+
+    if command == "install" {
+        let cmd_output = Command::new("apt")
+            .arg("install")
+            .arg("wireguard")
+            .output()
+            .unwrap();
+        if !&cmd_output.status.success() {
+            print!("There was something went wrong with install.");
+            println!("{}", String::from_utf8_lossy(&cmd_output.stderr));
+            return;
+        } else {
+            println!("Wireguard installed successfully");
+            return;
+        }
+    }
 }
