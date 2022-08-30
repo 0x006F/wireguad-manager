943bfddc82938892f32152503959806ee6df5687
giri <giri@brahmastra.dev>
2022-08-30 21:18:31 +0530

    refactor load and generate config

--- src/models/client.rs
+++ src/models/client.rs
@@ -1,4 +1,8 @@
-use std::fs::{create_dir_all, read_to_string, write};
+use std::{
+    fmt::format,
+    fs::{create_dir_all, metadata, read_to_string, write},
+    path::Path,
+};
 
 use serde::{Deserialize, Serialize};
 
@@ -13,6 +17,9 @@ pub struct ClientProfile {
     pub address: String,
     pub dns: Option<String>,
     pub endpoint: String,
+    server_public_key: String,
+    server_endpoint: String,
+    server_port: u32,
 }
 
 impl ClientProfile {
@@ -27,6 +34,9 @@ impl ClientProfile {
             dns: None,
             address: ip,
             endpoint: server_endpoint,
+            server_endpoint: "".to_owned(),
+            server_port: 51820,
+            server_public_key: "".to_owned(),
         };
         return client_config;
     }
@@ -42,57 +52,101 @@ impl ClientProfile {
         self.persist();
     }
 
-    pub fn persist(&self) {
+    fn save_config(&self) {
         let wireguard_install_path = "/home/giri/wireguard_mg";
-        create_dir_all(format!("{}/clients/{}", &wireguard_install_path, self.name)).unwrap();
+        create_dir_all(format!("{}/clients/", &wireguard_install_path)).unwrap();
 
-        // Save PSK
-        let psk_path = format!(
-            "{}/clients/{}/{}_psk",
-            wireguard_install_path, &self.name, &self.name
-        );
-
-        write(&psk_path, &self.psk).unwrap();
-
-        // Save Public Key
-        let pub_key_path = format!(
-            "{}/clients/{}/{}_pub",
-            wireguard_install_path, &self.name, &self.name
-        );
-
-        write(&pub_key_path, &self.public_key).unwrap();
-
-        // Save Private Key
-        let private_key_path = format!(
-            "{}/clients/{}/{}_key",
-            wireguard_install_path, &self.name, &self.name
-        );
-
-        write(&private_key_path, &self.private_key).unwrap();
+        let config_file_path = format!("{}/clients/{}.json", &wireguard_install_path, self.name);
+        println!("{:?}",self);
+        write(
+            config_file_path,
+            serde_json::to_string_pretty(self).unwrap(),
+        )
+        .unwrap()
     }
 
-    pub fn load(&mut self) {
+    pub fn persist(&self) {
+        self.save_config();
+        self.generate_conf(None);
+    }
+
+    pub fn load(client_name: String) -> Option<ClientProfile> {
         let wireguard_install_path = "/home/giri/wireguard_mg";
-        let psk_path = format!(
-            "{}/clients/{}/{}_psk",
-            wireguard_install_path, &self.name, &self.name
+        let client_config_path = format!("{}/clients/{}.json", wireguard_install_path, client_name);
+
+        let is_exist = metadata(&client_config_path).is_ok();
+        if !is_exist {
+            return None;
+        }
+
+        let config_contents_result = read_to_string(client_config_path);
+
+        match config_contents_result {
+            Err(_) => {
+                println!("Could not load config for {}", client_name);
+                return None;
+            }
+            Ok(contents) => {
+                let jsonified = serde_json::from_str::<ClientProfile>(&contents);
+
+                match jsonified {
+                    Err(_) => {
+                        println!("Was able to load config JSON for {}. But looks like that JSON is malformed.",client_name);
+                        return None;
+                    }
+                    Ok(client_config) => return Some(client_config),
+                }
+            }
+        }
+
+        // let psk_path = format!(
+        //     "{}/clients/{}/{}_psk",
+        //     wireguard_install_path, client_name, client_name
+        // );
+
+        // let private_key_path = format!(
+        //     "{}/clients/{}/{}_key",
+        //     wireguard_install_path, client_name, client_name
+        // );
+
+        // let pub_key_path = format!(
+        //     "{}/clients/{}/{}_pub",
+        //     wireguard_install_path, client_name, client_name
+        // );
+
+        // let psk = read_to_string(psk_path).unwrap();
+        // let private_key = read_to_string(private_key_path).unwrap();
+        // let public_key = read_to_string(pub_key_path).unwrap();
+        // return ClientProfile {
+        //     name: client_name,
+        //     public_key,
+        //     psk,
+        //     private_key,
+        //     address: (),
+        //     dns: (),
+        //     endpoint: (),
+        // };
+    }
+
+    pub fn generate_conf(&self, path: Option<String>) {
+        let wireguard_install_path = "/home/giri/wireguard_mg";
+
+        let conf_path = path.unwrap_or(format!(
+            "{}/clients/{}.conf",
+            wireguard_install_path, self.name
+        ));
+
+        let mut interface_block = format!(
+            "[Interface]\nPrivateKey = {}\nAddress = {}\n",
+            self.private_key, self.address
         );
 
-        let private_key_path = format!(
-            "{}/clients/{}/{}_key",
-            wireguard_install_path, &self.name, &self.name
-        );
+        if self.dns.is_some() {
+            interface_block.push_str(format!("DNS = {}", &self.dns.as_ref().unwrap()).as_str());
+        }
 
-        let pub_key_path = format!(
-            "{}/clients/{}/{}_pub",
-            wireguard_install_path, &self.name, &self.name
-        );
+        let peer_block = format!("[Peer]\nPublicKey = {}\nPresharedKey = {}\nAllowedIPs = {}\nEndpoint = {}\nPersistentKeepAlive = 25",self.server_public_key,self.psk,self.address,format!("{}:{}",self.server_endpoint,self.server_port));
 
-        let psk = read_to_string(psk_path).unwrap();
-        let private_key = read_to_string(private_key_path).unwrap();
-        let public_key = read_to_string(pub_key_path).unwrap();
-        self.psk = psk;
-        self.private_key = private_key;
-        self.public_key = public_key;
+        write(conf_path, format!("{}\n\n{}", interface_block, peer_block)).unwrap();
     }
 }
